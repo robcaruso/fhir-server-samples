ffcd7df03b5e21df5e12cbbcb0ac460355cc6754
Rob Caruso <rob.caruso@perspecta.com>
2019-07-05 15:00:57 -0400

    fixed nifi template path

--- deploy/scripts/Create-HealthConcourseAll.ps1
+++ deploy/scripts/Create-HealthConcourseAll.ps1
@@ -132,4 +132,4 @@ $fhirServerUrl = "https://${resourceGroupName}srvr.azurewebsites.net"
 New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -Name $deploymentNameMdmi -TemplateFile $templateFilePathMdmi;
 New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -Name $deploymentNameTransformation -TemplateFile $templateFilePathTransformation;
 New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -Name $deploymentNameTerminology -TemplateFile $templateFilePathTerminology;
-New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -Name $deploymentNameNifi -TemplateFile $templateFilePathNifi -aadAuthority $aadAuthority -aadServiceClientId $serviceClientId -aadServiceClientSecret $serviceClientSecret -fhirServerUrl $fhirServerUrl -storageAccountKey $storageAccountKey;
+New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -Name $deploymentNameNifi -TemplateFile $templateFilePathNifi -aadAuthority $aadAuthority -aadServiceClientId $serviceClientId -aadServiceClientSecret $serviceClientSecret -fhirServerUrl $fhirServerUrl;
